ace7c2ed99494cbb4bd901bdf19aed3ae7a98d74
Bakinator <Gustavbak@live.dk>
2024-12-09 14:35:48 +0100

    login page og tilhørende

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const stackRoutes = require('./routes/stackRoutes');
 const dateHelper = require('./public/helpers/dateHelper');
+const authRoutes = require('./routes/authRoutes');
+/*const session = require('express-session');*/
 
 const app = express();
 const PORT = 3000;
@@ -21,9 +23,22 @@ app.set('views', 'views');  // Specify views directory
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+/*const isAuthenticated = (req, res, next) => {
+    if (req.session && req.session.user) { // Replace with your session/cookie logic
+        next(); // User is authenticated, continue to the route
+    } else {
+        res.redirect('/auth/login'); // Redirect to the login page
+    }
+};*/
+
+module.exports = (req, res, next) => {
+    next(); // Allow access to the route
+};
 
 // Routes
 app.use('/', stackRoutes);
+app.use('/', pageRoutes);
+app.use ('/auth', authRoutes);
 
 // Start the server
 app.listen(PORT, () => {
