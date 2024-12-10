4f3d861c363ec81eb6d4735ffa18a5908132615c
AndersBuch <119923346+AndersBuch@users.noreply.github.com>
2024-12-10 15:43:43 +0100

    added stack route for groups.hbs

--- routes/stackRoutes.js
+++ routes/stackRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const stackController = require('../controllers/stackController');
 router.get('/stacks', stackController.getStacks);
+router.get('/groups', stackController.getStacks);
 router.get('/createStack', stackController.renderCreateStackForm);
 // router.get('/login', stackController.login);
 router.post('/stacks/create', stackController.createStack);
