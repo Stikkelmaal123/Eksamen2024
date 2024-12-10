1190aed21d75ca7c893242aa35f90157d104b7db
Mikkel Stal <Mikkel.Staal@outlook.dk>
2024-12-10 13:11:23 +0100

    Revert "removal of static"
    This reverts commit afef855b2319e58cd1d6c9e827ccf506a307dd84.

--- app.js
+++ app.js
@@ -3,26 +3,37 @@ const exphbs = require('express-handlebars');
 const stackRoutes = require('./routes/stackRoutes');
 const dateHelper = require('./public/helpers/dateHelper');
 const authRoutes = require('./routes/authRoutes');
+/*const session = require('express-session');*/
 
 const app = express();
 const PORT = 3000;
 
 app.engine('hbs', exphbs.engine({
-    extname: 'hbs',        
-    defaultLayout: 'main',  
-    layoutsDir: 'views/layouts/', 
+    extname: 'hbs',         // Use `.hbs` extension for Handlebars files
+    defaultLayout: 'main',  // Set default layout
+    layoutsDir: 'views/layouts/', // Specify layouts directory
     helpers: {
         formatDate: dateHelper.formatDate
     }
 }));
 app.set('view engine', 'hbs');
-app.set('views', 'views'); 
+app.set('views', 'views');  // Specify views directory
 
 // Middleware to serve static files
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+/*const isAuthenticated = (req, res, next) => {
+    if (req.session && req.session.user) { // Replace with your session/cookie logic
+        next(); // User is authenticated, continue to the route
+    } else {
+        res.redirect('/auth/login'); // Redirect to the login page
+    }
+};*/
 
+module.exports = (req, res, next) => {
+    next(); // Allow access to the route
+};
 
 // Routes
 app.use('/', stackRoutes);
--- routes/stackRoutes.js
+++ routes/stackRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const stackController = require('../controllers/stackController');
 router.get('/stacks', stackController.getStacks);
 router.get('/createStack', stackController.renderCreateStackForm);
+// router.get('/login', stackController.login);
 router.post('/stacks/create', stackController.createStack);
 
 
