afef855b2319e58cd1d6c9e827ccf506a307dd84
Mikkel Stal <Mikkel.Staal@outlook.dk>
2024-12-10 12:57:58 +0100

    removal of static

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const authController = require('../controllers/authController');
 const router = express.Router();
+const authController = require('../controllers/authController');
 
 router.get('/login', authController.getLoginPage); // Render login page
-router.post('/login', authController.postLogin); // Handle login form submission
+router.post('/login/auth', authController.postLogin); // Handle login form submission
 // router.get('/logout', authController.logout); // Handle logout
 // router.get('/password', authController.getForgotPasswordPage); // GET route for the forgot password page
 router.post('/password', authController.postForgotPassword); // POST route for handling the forgot password form
