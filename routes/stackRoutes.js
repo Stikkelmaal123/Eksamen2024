0282fb68b7ffc364140be92a47527120ef3ca0dc
AndersBuch <119923346+AndersBuch@users.noreply.github.com>
2024-12-14 13:42:24 +0100

    adas
    sad

--- routes/stackRoutes.js
+++ routes/stackRoutes.js
@@ -4,6 +4,8 @@ const stackController = require('../controllers/stackController');
 const groupController = require('../controllers/groupController');
 router.get('/stacks', stackController.getStacks);
 router.get('/groups', groupController.getAllGroups);
+router.get('/create-group', groupController.getCreateGroupPage);
+router.post('/create-group', groupController.createGroup);
 router.get('/createStack', stackController.renderCreateStackForm);
 router.post('/stacks/create', stackController.createStack);
 
