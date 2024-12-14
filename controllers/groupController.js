1a0b6f4bba0eff5f4b4fc6b53d291d13fe474fc9
AndersBuch <119923346+AndersBuch@users.noreply.github.com>
2024-12-14 12:17:54 +0100

    ads

--- controllers/groupController.js
+++ controllers/groupController.js
@@ -1,6 +1,26 @@
 const groupsModel = require('../models/groups'); // DB model for groups
 
 
+
+
+exports.createGroup = async (req, res) => {
+  try {
+      const { groupName, educationCheckbox, expirationDate} = req.body;
+
+      // Validate input
+      if (!groupName || !educationCheckbox || !expirationDate) {
+          return res.status(400).json({ error: 'Missing required fields' });
+
+      };
+  } catch (error) {
+      console.error('Error creating group:', error);
+      return res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+
+
+
 exports.getAllGroups = async (req, res) => {
   try {
       const groups = await groupsModel.getAllGroups();
